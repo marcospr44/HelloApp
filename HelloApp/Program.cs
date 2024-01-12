e61821b8dccde46c5faf937dea0063b998d116a0
Marcos <marcospaulorodrigues34@gmail.com>
2024-01-11 22:03:49 -0300

    Primeiro Commit

---
+++ HelloApp/Program.cs
@@ -0,0 +1,42 @@
+﻿//Imprime um texto na tela.
+using System.IO.Pipes;
+using System.Security.AccessControl;
+
+Console.WriteLine("Hello Word!");  
+
+Console.WriteLine("Seja bem vindo ao meu primeiro programa!");
+
+string nome = "Marcos Paulo";
+Console.WriteLine(nome + " é meu nome!");
+
+int idade = 44;
+
+decimal peso = 68;
+
+decimal altura = 1.67m;
+
+
+
+//Criação de Variáveis.
+
+int idade;
+
+
+Console.WriteLine("Qual é o seu nome?");
+
+// Lê o nome digitado pelo usuário e armazena na variável nome.
+
+nome = Console.ReadLine();
+
+Console.WriteLine("Prazer em conhecer" + nome + "!");
+
+Console.WriteLine("Qual é a sua idade");
+
+idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Você tem" + idade + "anos de idade.");
+
+
+
+
+
